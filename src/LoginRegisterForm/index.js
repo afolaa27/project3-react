15b741a003123748a6d4fa2f6bceb527659c1d70
Abdulkhaliq <afolaa27@uwgb.edu>
2020-02-21 13:48:10 -0600

    error changed

--- src/LoginRegisterForm/index.js
+++ src/LoginRegisterForm/index.js
@@ -98,9 +98,9 @@ class LoginRegisterForm extends Component{
 				{
 					this.props.wrongInfoReg
 					?
-					<small style={{color:'green'}}>You successfully signed up now login!</small>
-					:
 					<small style={{color:'red'}}>Try new details!</small>
+					:
+					<small style={{color:'green'}}>You successfully signed up now login!</small>
 				}
 				</Card>
 			</div>
